5790d0d3b1605c870c826259567791af708e42ea
Sebastian <s.kloesel@gmx.de>
2022-01-06 18:24:14 +0100

    Initial Commit

---
+++ Mail_Contact/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mail_Contact
+//
+//  Created by Sebastian Klösel on 06.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mail_Contact/Mail_ContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mail_ContactApp.swift
+//  Mail_Contact
+//
+//  Created by Sebastian Klösel on 06.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Mail_ContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
